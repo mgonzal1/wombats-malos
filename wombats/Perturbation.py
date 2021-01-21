d85e9beaee67bbc6546a19b0d20509af0d9894d8
franjmt <31972450+franjmt@users.noreply.github.com>
2021-01-20 21:17:25 -0500

    Create Perturbation.py

---
+++ wombats/Perturbation.py
@@ -0,0 +1,14 @@
+import random
+import numpy
+
+
+def A1_pert(encoder_output, n_pert):
+    # perturbar por fila/row
+    trial, neuron = encoder_output.shape
+    list_pert = list(np.arange(neuron-1))
+    x = random.sample(list_pert, n_pert)
+    encoder_output[:,x] = 0
+    new_encoder = encoder_output
+
+    return new_encoder, x
+
