a22c244bbdf9c0a58e57aecd678df6f4620d94c3
Alex Gonzalez <alexgb2010@gmail.com>
2021-01-27 18:05:09 -0800

    Update perturbation.py
    filled up todo.

--- wombats/perturbation.py
+++ wombats/perturbation.py
@@ -1,6 +1,24 @@
 import random
 import numpy as np
 
+# todo:
+# generalized to a2, a3
+# pertubation type:
+# ablation => set neurons to zero
+# white noise
+# poisson noise (if using spike counts not firing rate)
+# neural noise (get neuron specific firing probability) [needs neuron stats]
+# hyperactivate => set neuron to max fr [needs max fr as input]
+# scramble conections (permute columns); [needs how many to scramble]
+
+def pert_connections(connection_mat, pct_pert=0.2, pert_type="ablation"):
+    """
+    inputs:
+    returns:
+    """
+    
+    return pert_connection_mat, pert_idx 
+
 
 def a1_pert(encoder_output, n_pert):
     # perturbar por columna/neurona
