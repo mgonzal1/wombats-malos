6dccd6d3d8985807ba218bd6f6b61837eaa256ee
Alex Gonzalez <alexgb2010@gmail.com>
2021-01-27 17:42:41 -0800

    Update perturbation.py
    comment change

--- wombats/perturbation.py
+++ wombats/perturbation.py
@@ -3,7 +3,8 @@ import numpy as np
 
 
 def a1_pert(encoder_output, n_pert):
-    # perturbar por fila/row
+    # perturbar por columna/neurona
+    # todo: n_pert <= n_neurons
     n_trials, n_neurons = encoder_output.shape
     list_pert = list(np.arange(n_neurons-1))
     ablated_neurons = random.sample(list_pert, n_pert)
